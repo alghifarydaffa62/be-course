ca2386aa1ca0286dd90575905209cfd4c8af70d7
M Daffa A <daffa.alghifary62@gmail.com>
2025-06-30 17:16:43 +0700

    Auth me

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import * as Yup from "yup";
 import userModel from "../models/user.model";
 import { encrypt } from "../utils/encryption";
+import { generateToken } from "../utils/jwt";
+import { IReqUser } from "../middleware/authMiddleware";
 
 type TRegister = {
     fullname: string;
@@ -85,12 +87,35 @@ export default {
                 });
             }
 
+            const token = generateToken({
+                id: userByIdentifier._id,
+                role: userByIdentifier.role
+            })
+
             res.status(200).json({
                 message: "Login success",
-                data: userByIdentifier
+                data: token
             })
 
         } catch(error) {
+            const err = error as unknown as Error;
+            res.status(400).json({
+                message: err.message,
+                data: null
+            })
+        }
+    },
+
+    async me(req: IReqUser, res: Response) {
+        try {
+            const user = req.user;
+            const result = await userModel.findById(user?.id);
+
+            res.status(200).json({
+                message: "Success get user profile",
+                data: result
+            })
+        } catch(error) {    
             const err = error as unknown as Error;
             res.status(400).json({
                 message: err.message,
