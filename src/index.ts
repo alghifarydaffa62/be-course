7c0e9d57b08a0882a3fb6aadfcaec6e56e6f0343
M Daffa A <daffa.alghifary62@gmail.com>
2025-06-30 11:36:28 +0700

    Auth register

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import router from "./routes/api";
+import bodyParser from "body-parser";
 
 const app = express();
 
 const PORT = 3000;
 
+app.use(bodyParser.json());
 app.use('/api', router);
+
 
 app.listen(PORT, () => {
     console.log(`server is running on http://localhost:${PORT}`)
